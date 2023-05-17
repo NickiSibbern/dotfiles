02cf8a859a52e54e2c0685dcb1db3cd6dffaf0e4
Nicki Sibbern <5722216+NickiSibbern@users.noreply.github.com>
2023-05-18 00:12:36 +0200

    rectangle and brew install improvements
    install rectangle app
    change iterm to --cask install
    reorder install order

--- install.sh
+++ install.sh
@@ -1,16 +1,20 @@
+# CLI tools
 brew install zsh
-brew install pure
 brew install zsh-autosuggestions
 brew install zsh-syntax-highlighting
 brew install zsh-history-substring-search
+brew install pure
 brew install fzf
 brew install direnv
 brew install tree
 brew install jq
-brew install iterm2
 brew install git
 brew install git-lfs
 brew install curl
 
-#Copy configs
+# Applications
+brew install --cask iterm2
+brew install --cask rectangle
+
+# Copy configs
 cp -R ./config/. ~
