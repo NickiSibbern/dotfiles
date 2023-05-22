ed7b1d44872481b231cf99db9e993e168de625e8
Nicki Sibbern <5722216+NickiSibbern@users.noreply.github.com>
2023-05-22 21:19:03 +0200

    Replaced install.sh with OS specific script instead

--- macOS.sh
+++ macOS.sh
@@ -15,6 +15,9 @@ brew install jandedobbeleer/oh-my-posh/oh-my-posh
 brew install --cask iterm2
 brew install --cask rectangle
 
+# backup existing .zshrc files
+mv ~/.zshrc ~/.zshrc_bak
+
 # Oh my zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
